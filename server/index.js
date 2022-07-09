6641144b156178c8557a202deca79a87dcd5cf11
jatoledoz <jorgeaugusto.ts@gmail.com>
2022-07-09 20:00:25 -0300

    Initial commit - Functional TodoList App using MEVN

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const TodoListRoutes = require('./routes/api/Todolist')
+const path = require('path')
+require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB database Connected...'))
+  .catch((err) => console.log(err))
+
+
+app.use('/api/todolist', TodoListRoutes)
+app.listen(process.env.PORT, () => console.log(`App listening at http://localhost:${process.env.PORT}`))
